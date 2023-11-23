ac2e1d3db58398d2fa0904cf11169427d597b652
ATrinidadBarrientos <137270482+AlexTrinidadB@users.noreply.github.com>
2023-11-23 16:56:01 -0500

    Update streamlit_app.py
    icono agregado

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,11 @@ data = pd.read_excel('fashionsales.xlsx', engine='openpyxl')
 
 # Configuración del título y la descripción
 st.title('Datos de Ventas de ropa en Australia')
-st.write('Este es un dashboard simple para analizar datos de ventas para la ayuda de toma de decisiones.')
+st.write('Este es un dashboard para analizar datos de ventas para la ayuda de toma de decisiones.')
+
+icon_url = 'icono.jpg'  # Reemplaza 'URL_DEL_ICONO' con la URL de la imagen del icono
+st.image(icon_url, width=100) 
+
 
 # Filtrar los datos por mes
 selected_month = st.selectbox('Seleccionar mes:', data['Month'].unique())
