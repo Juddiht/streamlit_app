3c6a9943361a8924ed3b4c0c1a1a8644bc9aa92c
ATrinidadBarrientos <137270482+AlexTrinidadB@users.noreply.github.com>
2023-11-23 16:29:46 -0500

    Hola_1.py
    Configuracion de fechas

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,9 @@ import openpyxl
 # Cargar los datos
 data = pd.read_excel('fashionsales.xlsx', engine='openpyxl')
 
+# Convertir la columna 'Month' al formato de fecha
+data['Month'] = pd.to_datetime(data['Month'], format='%d/%m/%Y', errors='coerce')
+
 # Configuración del título y la descripción
 st.title('Datos de Ventas de Ropa en Australia')
 st.write('Este es un dashboard simple para analizar datos de ventas para la ayuda de toma de decisiones.')
@@ -38,13 +41,18 @@ st.bar_chart(manager_sales)
 
 # Selector de mes para el cuarto gráfico
 selected_month_time_series = st.selectbox('Seleccionar mes para Ventas a lo largo del tiempo:', data['Month'].dt.to_period('M').unique())
-time_series_data = data.groupby(data['Month'].dt.to_period('M'))['Sales'].sum()
 filtered_data_time_series = data[data['Month'].dt.to_period('M') == selected_month_time_series]
 
 # Gráfico de líneas para las ventas a lo largo del tiempo
 st.subheader(f'Ventas a lo largo del tiempo para {selected_month_time_series}')
+time_series_data = data.groupby(data['Month'].dt.to_period('M'))['Sales'].sum()
 st.line_chart(time_series_data)
 
 # Selector de mes para la tabla de resumen
 selected_month_summary = st.selectbox('Seleccionar mes para Resumen de Ventas:', data['Month'].dt.to_period('M').unique())
 filtered_data_summary = data[data['Month'].dt.to_period('M') == selected_month_summary]
+
+# Tabla de resumen
+st.subheader(f'Resumen de Ventas para {selected_month_summary}')
+summary = filtered_data_summary.groupby([data['Month'].dt.to_period('M'), 'Category'])['Sales'].sum().unstack()
+st.dataframe(summary)
